e379d9b0d9ea7da36a938a48b6e2551a1b96cfbc
jacob17baker <jacobbaker17@live.com>
2017-03-09 04:35:09 -0800

    testing

---
+++ php/Threads.php
@@ -0,0 +1,49 @@
+<?php
+	//Start the session
+	session_start();
+	include 'Functions';
+?>
+<DOCTYPE html>
+<html>
+    <head>  
+        <link rel="stylesheet" type="text/css" href="MainStyles">
+        <title>Project Nibbles and Bits</title>
+        <script src="MainJS"></script>
+    </head>
+    
+    <body>
+        <h1>Project Nibbles and Bits</h1>
+		<?php loginDisplay($_SESSION["userName"]); ?>
+
+        <ul id="Nav_Bar">
+            <li><a href="Index">Home</a></li>
+            <li><a href="Recipes">Recipes</a></li>
+            <li><a href="MyRecipes">My Recipes</a></li>
+            <li><a href="Categories">Categories</a></li>
+            <li><a href="Forums">Forums</a></li>
+            <li><a href="TipsAndTricks">Tips and Tricks</a></li>
+        </ul>
+		<?php
+			//retrieves the header
+			$recipe = connectServer("CALL topicFound('".$_GET["topic_id"]."')", $_SESSION["userName"]);
+			while($row = mysqli_fetch_assoc($recipe))
+			{
+				echo "<h2>" . $row["recipe_name"] . "</h2>";
+			}
+		?>
+		<form action="Forums" method="get">
+        	<input type="search" name="topic"> 
+			<input id="Recipe_Submit" type="submit" value="Find Topics"> 
+		</form>
+		<ul id="search">
+			<?php
+				$threads = connectServer("CALL getThreads('".$_GET["topic_id"]."');", $_SESSION["userName"]);
+				while($row = mysqli_fetch_assoc($threads))
+				{
+						echo "<button class='search' onclick=location.href='Thread?thread_id=" . $row["thread_id"] . "'>" . $row["thread_name"] . "</button>";
+						echo "<br>";
+				}
+			?>
+		</ul>
+	</body>
+</html>

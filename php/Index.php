d3e1ddf4864f21d2188655e768b1647497491ad6
jacob17baker <jacobbaker17@live.com>
2017-03-16 12:25:24 -0700

    gitit

--- php/Index.php
+++ php/Index.php
@@ -4,8 +4,8 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="author" content="Nathan Hoffman">
-	<link rel="stylesheet" type="text/css" href="MainStyles">
-    <link rel="stylesheet" type="text/css" href="SearchStyles">
+	<link rel="stylesheet" type="text/css" href="MainStyles.css">
+    <link rel="stylesheet" type="text/css" href="SearchStyles.css">
 </head>
 
 <body>
